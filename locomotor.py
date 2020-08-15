f49c7a31e75c51b6b5cae37e73d9e24450125a42
Jon Palacio <bangarangler@gmail.com>
2020-08-15 16:29:00 -0400

    fresh to try agian

--- locomotor.py
+++ locomotor.py
@@ -21,37 +21,13 @@ if __name__ == "__main__":
         This function will...
         1. checkout the branch name provided
         """
-        # already_a_branch = False
-        # existing_branches = []
         if branch in my_repo.branches:
             print(f"{branch} is already a branch")
             my_repo.git.checkout(branch)
         else:
             print(f"{branch} is not a branch yet")
             my_repo.git.checkout("-b", branch)
-        # for sing_branch in my_repo.branches:
-            # print(f"branch : {branch}")
-            # print(f"sing_branch : {str(sing_branch)}")
-            # print(
-            #     f"sing_branch is {str(sing_branch)} while branch is {branch}")
-        #     existing_branches = []
-        #     if str(sing_branch) == branch:
-        #         print("setting already_a_branch to True")
-        #         already_a_branch = True
-        #         # my_repo.git.checkout(branch)
-        #         # return
-        #     else:
-        #         print("setting already_a_branch to False")
-        #         already_a_branch = False
-        #         # my_repo.git.checkout("-b", branch)
-        #         # return
-        # if (already_a_branch == True):
-        #     print("inside already_a_branch == True")
-        #     # my_repo.git.checkout(branch)
-        # else:
-        #     print("inside else already_a_branch == False")
-        #     # my_repo.git.checkout("-b", branch)
-        # print("pulling...")
+        print("pulling...")
         # # my_repo.remotes.origin.pull()
 
     with my_repo.config_writer() as git_config:
@@ -64,7 +40,7 @@ if __name__ == "__main__":
     if not my_repo.bare:
         print(f"Repo at {repo_path} successfully loaded.")
         print_repo(my_repo)
-        locomotor("master")
+        locomotor("testPythonBranch")
 
     else:
         print("Could not load repo")
